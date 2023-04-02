44fc3b9ca2efe853c2b85c44a21cb503cdf8fd77
antoine311200 <antoine311200@gmail.com>
2023-04-02 18:21:31 +0200

    Convert to torch tensor and return None on corrupted images

--- core/dataset.py
+++ core/dataset.py
@@ -8,7 +8,7 @@ from tifffile import tifffile as tif
 import numpy as np
 import pandas as pd
 import os
-
+import time
 
 def mask(img_mask):
     """Transform an S2 mask (values between 1 and 9) to float32 binary.
@@ -35,12 +35,14 @@ def read_img(data_path: str, name: str):
 
     # Normalize LAI
     lai_min, lai_max = result["lai"].min(), result["lai"].max()
-    result["lai"] = (result["lai"] - lai_min) / (lai_max - lai_min)
+    if lai_max > lai_min:
+        result["lai"] = (result["lai"] - lai_min) / (lai_max - lai_min)
 
-    # Normalize VV and VH
-    result["vv"] /= -30
-    result["vh"] /= -30
-
+        # Normalize VV and VH
+        result["vv"] /= -30
+        result["vh"] /= -30
+    else:
+        return None
     return result
 
 
@@ -56,7 +58,7 @@ class LNBDataset(Dataset):
         self.stackify = stackify
 
     def __len__(self):
-        return len(self.data)
+        return self.series.shape[0]
 
     def __getitem__(self, idx):
         """Get a sample and its prediction.
@@ -67,9 +69,16 @@ class LNBDataset(Dataset):
         Returns:
             list: Sample & prediction
         """
+        # start = time.time()
+
         t_2 = read_img(self.data_path, self.series["0"][idx])
         t_1 = read_img(self.data_path, self.series["1"][idx])
         t = read_img(self.data_path, self.series["2"][idx])
+
+        # print("Read imgs", time.time() - start)
+
+        if t_2 is None or t_1 is None or t is None:
+            return None
 
         sample = {"t_2": t_2, "t_1": t_1, "t": {"vv": t["vv"], "vh": t["vh"]}}
         prediction = {"lai": t["lai"], "lai_mask": t["lai_mask"]}
@@ -82,12 +91,15 @@ class LNBDataset(Dataset):
                     *sample["t_1"].values(),
                     *sample["t"].values(),
                 ]
-            ).transpose(1, 2, 0)
+            )#.transpose(1, 2, 0)
 
             # Stackify the prediction dictionary into a (256, 256, 1) array
             prediction = np.array(prediction["lai"])[..., np.newaxis]
 
-        return sample, prediction
+        # if self.transform:
+        #     sample = self.transform(sample)
+
+        return torch.tensor(sample), torch.tensor(prediction)
 
     def plot(self, idx):
         sample, prediction = self.__getitem__(idx)
