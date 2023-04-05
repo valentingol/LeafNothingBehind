b68b8ea5c6f9aa42515b05fd843523a603903c55
Clement Wang <clementwang2001@gmail.com>
2023-04-05 12:03:23 +0200

    feat: training

---
+++ clem_pipeline/models/custom_models.py
@@ -0,0 +1,16 @@
+from clem_pipeline.models.unet_module import UNet
+from torch import nn 
+import torch 
+
+class OnlyS1Idea1(nn.Module):
+    def __init__(self, bilinear=False):
+        super(OnlyS1Idea1, self).__init__()
+        self.unet = UNet(6, 1, bilinear)
+
+    def forward(self, t2s1, t1s1, ts1, t2s2, t1s2, t2s2_mask, t1s2_mask):
+        """Return a tensor of shape (batch_size, 256, 256) corresponding to the LAI of each image"""
+        img_size = t2s1.shape[2]
+        return self.unet(torch.cat([t2s1, t1s1, ts1], dim=1)).view(-1, img_size, img_size)
+        
+
+
--- clem_pipeline/training.py
+++ clem_pipeline/training.py
@@ -1,42 +1,121 @@
 import torch 
+from torch import nn 
 from data.dataloader import TrainDataset
 
 
+class MetricTracker(object):
+    """Computes and stores the average and current value"""
 
-def train_loop(dataloader, model, loss_fn, optimizer, device):
-    size = len(dataloader.dataset)
-    for batch, (X, y) in enumerate(dataloader):
-        # Compute prediction error
-        pred = model(X)
-        loss = loss_fn(pred, y)
+    def __init__(self):
+        self.reset()
 
-        # Backpropagation
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+
+class WeightedMSE(nn.Module):
+    def __init__(self):
+        super(WeightedMSE, self).__init__()
+
+    def forward(self, pred, target, weight):
+        return torch.mean(weight * (pred - target) ** 2)
+    
+
+def train_loop(dataloader, model, criterion, optimizer, loss_tracker, device):
+    model.train()
+
+    for data, timestamp in dataloader:
+        data.to(device)
+        timestamp.to(device)
+
+        t2s1, t1s1, ts1, t2s2, t1s2, ts2, t2s2_mask, t1s2_mask, ts2_binary_mask = parse_training_tensor(data)
+
+        pred = model(t2s1, t1s1, ts1, t2s2, t1s2, t2s2_mask, t1s2_mask)
+
+        loss = criterion(pred, ts2, ts2_binary_mask)
+
+        loss_tracker.update(loss.item(), data.size(0))
+
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
 
-        if batch % 100 == 0:
-            loss, current = loss.item(), batch * len(X)
-            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+def val_loop(dataloader, model, criterion, loss_tracker, device):
+    model.eval()
+
+    for data, timestamp in dataloader:
+        data.to(device)
+        timestamp.to(device)
+
+        t2s1, t1s1, ts1, t2s2, t1s2, ts2, t2s2_mask, t1s2_mask, ts2_binary_mask = parse_training_tensor(data)
+
+        with torch.no_grad():
+            pred = model(t2s1, t1s1, ts1, t2s2, t1s2, t2s2_mask, t1s2_mask)
+            loss = criterion(pred, ts2, ts2_binary_mask)
+
+        loss_tracker.update(loss.item(), data.size(0))
 
 if __name__ == "__main__":
     from clem_pipeline.utils import parse_training_tensor
+    from clem_pipeline.models.custom_models import OnlyS1Idea1
 
-    DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
     use_wandb = False
 
-    print(f"Using {DEVICE} device")
+    parameters = {
+        "batch_size": 4,
+        "lr": 1e-3,
+        "epochs": 10,
+        "device": "cuda" if torch.cuda.is_available() else "cpu",
+    }
+
+    if use_wandb:
+        import wandb
+
+        wandb.init(project="PROJECT_NAME", entity="clem", config=parameters)
+
+        parameters = wandb.config # for sweep
+
+
+    print(f"Using {parameters['device']} device")
 
     dataset = TrainDataset(dataset_path='../assignment-2023', csv_data='image_series.csv',
                             csv_grid='../assignment-2023/square.csv', grid_augmentation=True, all_masks=True)
-    dataloader = torch.utils.data.DataLoader(dataset, batch_size=4, shuffle=True,
+    train_dataloader = torch.utils.data.DataLoader(dataset, batch_size=4, shuffle=True,
                                                 num_workers=4)
-    
+    val_dataloader = torch.utils.data.DataLoader(dataset, batch_size=4, shuffle=True,
+                                                num_workers=4)
 
-    for data, timestamp in dataloader:
-        print(data.shape)
-        print(timestamp.shape)
-        for element in parse_training_tensor(data):
-            print(element.shape)
-            
-        break    
+    model = OnlyS1Idea1().to(parameters['device'])
+    criterion = WeightedMSE()
+    train_loss_tracker = MetricTracker()
+    val_loss_tracker = MetricTracker()
+    optimizer = torch.optim.Adam(model.parameters(), lr=parameters['lr'])
+
+    best_val_loss = float("inf")
+
+    for epoch in range(parameters['epochs']):
+
+        print(f"Epoch {epoch}")
+        train_loss_tracker.reset()
+        val_loss_tracker.reset()
+
+        train_loop(train_dataloader, model, criterion, optimizer, train_loss_tracker, parameters['device'])
+        val_loop(val_dataloader, model, criterion, val_loss_tracker, parameters['device'])
+
+        
+        if val_loss_tracker.avg < best_val_loss:
+            best_val_loss = val_loss_tracker.avg
+            # save model ?
+
+        # wandb
+        print(f"ep:{epoch}/{parameters['epochs']}| train loss: {train_loss_tracker.avg:.4f} | val loss: {val_loss_tracker.avg:.4f} | best val loss: {best_val_loss:.4f}")
+
