b98d7aec912074b32ccf67812de93cc6187b2f44
Valentin Goldité <valentin.goldite@gmail.com>
2023-04-24 01:32:47 +0200

    🚀 Add unified training functions

--- lnb/training/trainer.py
+++ lnb/training/trainer.py
@@ -1,27 +1,26 @@
-"""Generic trainer for LNB models."""
-import argparse
-from collections import defaultdict
-from typing import Callable, Dict, Optional, Tuple
+"""Training functions for Scandium."""
 import os
+from time import time
+from typing import Dict, List, Tuple
 
 import numpy as np
 import torch
+import wandb
 import yaml
 from torch import nn
 from torch.optim.lr_scheduler import MultiStepLR
 from torch.utils.data import DataLoader
-from tqdm import tqdm, trange
 
-import wandb
-from lnb.architecture.models import Scandium
+from lnb.architecture import models as models_module
 from lnb.data.dataset import LNBDataset
+from lnb.training.log_utils import get_time_log
 from lnb.training.metrics import mse_loss
 
 ParsedDataType = Dict[str, Dict[str, torch.Tensor]]
 
 
 def mask_fn(img_mask: np.ndarray) -> np.ndarray:
-    """Mask processing function.
+    """Process mask function.
 
     Transform an S2 mask (values between 1 and 9) to float32.
     Channels are last dimension.
@@ -36,19 +35,20 @@ def mask_fn(img_mask: np.ndarray) -> np.ndarray:
     return np.stack([metric_mask, mask_2, mask_3, mask_4, mask_5, mask_6], axis=-1)
 
 
-def parse_data_device(data: torch.Tensor, device: torch.device) -> ParsedDataType:
-    """Parse data from dataloader and put it on device.
-
-    Parse data
-    Data dims: (batch, time, channels, h, w)
-    Channels:
-      0: LAI
-      1: LAI mask (metric)
-      2 -> -3: LAI mask (other channels)
-      -2: VV
-      -1: VH
-    """
-    data, glob = data
+def parse_data_device(
+    data: torch.Tensor,
+    glob: torch.Tensor,
+    device: torch.device,
+) -> ParsedDataType:
+    """Parse data from dataloader and put it on device."""
+    # Parse data
+    # Data dims: (batch, time, channels, h, w)
+    # Channels:
+    #   0: LAI
+    #   1: LAI mask (metric)
+    #   2 -> -3: LAI mask (other channels)
+    #   -2: VV
+    #   -1: VH
     in_lai = data[:, :2, 0:1].to(device)
     lai_target = data[:, 2, 0:1].to(device)
     in_mask_lai = data[:, :2, 1:-2].to(device)
@@ -67,240 +67,198 @@ def parse_data_device(data: torch.Tensor, device: torch.device) -> ParsedDataTyp
     return parsed_data
 
 
-class Trainer:
-    """Base trainer class."""
-
-    def __init__(
-        self,
-        model: nn.Module,
-        dataloaders: Dict[str, DataLoader],
-        process_func: Callable,
-        device: torch.device,
-        config: Optional[Dict] = None,
-    ) -> None:
-        self.model = model.to(device)
-        self.dataloders = dataloaders
-        self.process_func = process_func
-        self.config = config
-        self.device = device
-
-        self.optimizer, self.scheduler = self.configure_optimizers()
-
-    def configure_optimizers(
-        self,
-    ) -> Tuple[torch.optim.Optimizer, torch.optim.lr_scheduler._LRScheduler]:
-        """Configure the optimizers and the learning rate schedulers.
-
-        Returns
-        -------
-        Tuple[torch.optim.Optimizer, torch.optim.lr_scheduler._LRScheduler]
-            Optimizer and scheduler
-        """
-        train_config = self.config["train"]
-
-        n_epochs = train_config["n_epochs"]
-        lr_decay = train_config["learning_rate_decay"]
-        lr_n_mult = train_config["learning_rate_n_mult"]
-
-        milestones = [
-            int(epoch) for epoch in np.linspace(0, n_epochs, lr_n_mult + 1)
-        ][1:]
-
-        optimizer = torch.optim.Adam(
-            self.model.parameters(), lr=train_config["learning_rate"],
-        )
-        scheduler = MultiStepLR(optimizer, milestones=milestones, gamma=lr_decay)
-        return optimizer, scheduler
-
-    def train_step(self, parsed_data: ParsedDataType, loss_names: list) -> Tuple:
-        """Make a train step.
-
-        Parameters
-        ----------
-        data ParsedDataType:
-            Parsed data. Contains 'input_data' and 'target_data' keys.
-        loss_names (list): Loss names
-
-        Returns
-        -------
-            Tuple: Losses
-        """
-        self.optimizer.zero_grad()
-        predictions = self.model(**parsed_data["input_data"])
-        losses = [
-            mse_loss(lai_pred=pred, **parsed_data["target_data"])
-            for pred in predictions
-            if pred is not None
-            and all(pred.shape == d.shape for d in parsed_data["target_data"].values())
-        ]
-
-        losses[0].backward()
-        self.optimizer.step()
-
-        return {
-            loss_name: loss
-            for loss_name, loss in zip(loss_names, losses)
-            if loss is not None
-        }
-
-    def validation_step(self, data: ParsedDataType, loss_names: list) -> Tuple:
-        """Make a validation step.
-
-        Parameters
-        ----------
-        data : ParsedDataType
-            Parsed data. Contains 'input_data' and 'target_data' keys.
-        loss_names : list
-            Loss names
-
-        Returns
-        -------
-            Tuple: Losses
-        """
-        with torch.no_grad():
-            predictions = self.model(**data["input_data"])
-            losses = [
-                mse_loss(lai_pred=pred, **data["target_data"])
-                for pred in predictions
-                if pred is not None
-                and all(pred.shape == d.shape for d in data["target_data"].values())
-            ]
-
-        return {
-            loss_name: loss
-            for loss_name, loss in zip(loss_names, losses)
-            if loss is not None
-        }
-
-    def general_step(
-        self,
-        name: str,
-        dataloader: DataLoader,
-        progress_bar,
-        config: Dict,
-        subname: str = None,
-    ) -> None:
-        """General step which can be used for train step and val steps.
-
-        Args:
-            name (str): _description_
-            dataloader (DataLoader): _description_
-            progress_bar (_type_): _description_
-            config (Dict): _description_
-
-        Returns
-        -------
-            _type_: _description_
-        """
-        all_losses = defaultdict(list)
-        all_mean_losses = defaultdict(list)
-        all_std_losses = defaultdict(list)
-
-        pbar = tqdm(
-            enumerate(dataloader),
-            desc=name.capitalize(),
-            total=len(dataloader),
-        )
-        for idx_batch, data in pbar:
-            processed_data = self.process_func(data, self.device)
-            losses = getattr(self, f"{name}_step")(
-                processed_data, loss_names=["mse_loss"],
+def train_step(
+    model: nn.Module,
+    optimizer: torch.optim.Optimizer,
+    parsed_data: ParsedDataType,
+    **kwargs: Dict,
+) -> Tuple:
+    """Perform a training step."""
+    # Forward pass and loss computation
+    optimizer.zero_grad()
+    lai_pred, other_pred = model(**parsed_data["input_data"])
+    loss = mse_loss(lai_pred=lai_pred, **parsed_data["target_data"])
+    if kwargs["interm_supervis"]:
+        if kwargs["weight_2"] and kwargs["weight_3"]:
+            # Strontium supervisation
+            lai_pred2, lai_pred3 = other_pred
+            loss2 = mse_loss(
+                lai_pred=lai_pred2,
+                **parsed_data["target_data"],
             )
-
-            if subname:
-                name = f"{name}/{subname}"
-
-            for loss_name, loss_value in losses.items():
-                loss_value = loss_value.detach().cpu().numpy()
-                # Clip loss value to [0, 1]
-                loss_value = np.clip(loss_value, 0, 1)
-                all_losses[loss_name].append(loss_value)
-                wandb.log({f"{name}/{loss_name}": loss_value})
-
-            pbar.set_postfix(
-                {
-                    f"{name}/{loss_name}": loss_value
-                    for loss_name, loss_value in losses.items()
-                },
+            loss3 = mse_loss(
+                lai_pred=lai_pred3,
+                **parsed_data["target_data"],
             )
-
-        # Wandb log mean losses
-        for loss_name, loss_values in all_losses.items():
-            mean_loss = np.mean(loss_values)
-            std_loss = np.std(loss_values)
-
-            all_mean_losses[loss_name] = mean_loss
-            all_std_losses[loss_name] = std_loss
-
-            wandb.log({f"{name}/mean_{loss_name}": mean_loss})
-            wandb.log({f"{name}/std_{loss_name}": std_loss})
-
-        progress_bar.set_postfix(
-            {
-                f"{name}/mean_{loss_name}": loss_value
-                for loss_name, loss_value in all_mean_losses.items()
-            },
-        )
-
-        return all_mean_losses, all_std_losses, all_losses
-
-    def run(self):
-        run_id = self.config["run_id"]
-        train_config = self.config["train"]
-        n_epochs = train_config["n_epochs"]
-
-        defaultdict(list)
-        defaultdict(list)
-
-        epoch_pbar = trange(n_epochs, desc="Epochs")
-        for epoch in epoch_pbar:
-            epoch_pbar.set_description(f"Epoch {epoch+1}/{n_epochs}")
-
-            # Train
-            self.model.train()
-            train_mean_losses, train_std_losses, all_losses = self.general_step(
-                "train", self.dataloders["train"], epoch_pbar, train_config,
+            loss_total = loss + kwargs["weight_2"] * loss2 + kwargs["weight_3"] * loss3
+        else:
+            lai_target_interm = torch.cat(
+                [
+                    parsed_data["input_data"]["in_lai"],
+                    parsed_data["target_data"]["lai_target"].unsqueeze(1),
+                ],
+                dim=1,
             )
+            lai_target_mask_interm = torch.cat(
+                [
+                    parsed_data["input_data"]["in_mask_lai"][:, :, 0:1],
+                    parsed_data["target_data"]["lai_target"].unsqueeze(1),
+                ],
+                dim=1,
+            )
+            loss2 = mse_loss(
+                lai_pred=other_pred,
+                lai_target=lai_target_interm,
+                lai_target_mask=lai_target_mask_interm,
+            )
+            loss_total = loss + loss2
+    else:
+        loss2 = None
+        loss_total = loss
+    # Backward pass and weight update
+    loss_total.backward()
+    optimizer.step()
+    return loss, loss2
 
-            # Validation
-            self.model.eval()
-            for val_name, val_dataloader in self.dataloders["validation"].items():
-                torch.cuda.empty_cache()
-                train_mean_losses, train_std_losses, all_losses = self.general_step(
-                    "validation",
-                    val_dataloader,
-                    epoch_pbar,
-                    None,
-                    subname=val_name.split("_")[1],
-                )
 
-            torch.cuda.empty_cache()
-            self.scheduler.step()
+def valid_step(model: nn.Module, parsed_data: ParsedDataType) -> torch.Tensor:
+    """Perform a training step."""
+    # Forward pass and loss computation
+    with torch.no_grad():
+        pred_lai = model(**parsed_data["input_data"])[0]
+        loss = mse_loss(pred_lai, **parsed_data["target_data"])
+    return loss
 
-            if (epoch + 1) % train_config["save_interval"] == 0:
-                torch.save(
-                    model.state_dict(),
-                    f"../models/{self.model.__name__.lower()}/"
-                    f"{run_id}/{run_id}_ep{epoch + 1}.pth",
+
+# pylint: disable=too-many-locals
+def train_val_loop(
+    config: Dict,
+    model: nn.Module,
+    train_dataloader: DataLoader,
+    val_dataloaders: List[DataLoader],
+    device: torch.device,
+    archi_name: str,
+) -> None:
+    """Training and validation loop."""
+    # Save config
+    run_id = config["run_id"]
+    os.makedirs(f"../models/{archi_name}/{run_id}", exist_ok=True)
+    with open(
+        f"../models/{archi_name}/{run_id}/config.yaml",
+        "w",
+        encoding="utf-8",
+    ) as cfg_file:
+        yaml.dump(dict(wandb.config), cfg_file)
+    # Get training config params
+    train_config = config["train"]
+    n_epochs = train_config["n_epochs"]
+    n_batch = len(train_dataloader)
+    lr_decay = train_config["learning_rate_decay"]
+    lr_n_mult = train_config["learning_rate_n_mult"]
+    # Optimizer and scheduler
+    optimizer = torch.optim.Adam(model.parameters(), lr=train_config["learning_rate"])
+    milestones = [int(epoch) for epoch in np.linspace(0, n_epochs, lr_n_mult + 1)][1:]
+    scheduler = MultiStepLR(optimizer, milestones=milestones, gamma=lr_decay)
+
+    start_t = time()
+    for epoch in range(n_epochs):
+        # Training
+        model = model.train()
+        epoch_start_t = time()
+        train_losses = []
+        i_batch = 0  # iteration number in current epoch
+        for data, glob in train_dataloader:
+            i_batch += 1
+            # Parse data and put it on device
+            parsed_data = parse_data_device(data, glob, device)
+            loss, loss2 = train_step(
+                model,
+                optimizer,
+                parsed_data,
+                interm_supervis=train_config["interm_supervis"],
+                weight_2=train_config["weight_2"],
+                weight_3=train_config["weight_3"],
+            )
+            train_losses.append(loss.item())
+            # Logs
+            wandb.log({"train loss": loss.item()})
+            if loss2 is not None:
+                wandb.log({"train loss2": loss2.item()})
+            if i_batch % train_config["log_interval"] == 0:
+                current_t = time()
+                eta_str, eta_ep_str = get_time_log(
+                    current_t,
+                    start_t,
+                    epoch_start_t,
+                    i_batch,
+                    epoch,
+                    n_batch,
+                    n_epochs,
                 )
                 print(
-                    f"Model saved to ../models/{self.model.__name__.lower()}/{run_id}/"
-                    f"{run_id}_ep{epoch + 1}.pth",
+                    f"train loss batch: {loss.item():.4f} - eta epoch {eta_ep_str}"
+                    f"- eta {eta_str}     ",
+                    end="\r",
                 )
 
+        # Epoch train logs
+        mean_train_loss = sum(train_losses) / len(train_losses)
+        wandb.log({"mean train loss": mean_train_loss})
+        print(f"\nEpoch {epoch + 1}/{n_epochs}, mean train loss {mean_train_loss:.4f}")
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--config_path", type=str, required=False, default="config/scandium/base.yaml",
-    )
-    args = parser.parse_args()
+        # Validation
+        model = model.eval()
+        for val_dataloader in val_dataloaders:
+            # Free unused VRAM
+            torch.cuda.empty_cache()
+            val_name = val_dataloader.dataset.name
+            n_batch_val = len(val_dataloader)
+            valid_losses = []
+            i_batch = 0
+            for data, glob in val_dataloader:
+                i_batch += 1
+                # Parse data and put it on device
+                parsed_data = parse_data_device(data, glob, device)
+                loss = valid_step(model, parsed_data)
+                valid_losses.append(loss.item())
+                # Logs
+                if i_batch % train_config["log_interval"] == 0:
+                    print(
+                        f"Validation in progress... batch {i_batch}/{n_batch_val}  ",
+                        end="\r",
+                    )
 
-    with open(args.config_path, encoding="utf-8") as cfg_file:
-        config = yaml.safe_load(cfg_file)
+            # Epoch validation logs
+            mean_valid_loss = sum(valid_losses) / len(valid_losses)
+            wandb.log({f"mean valid loss {val_name}": mean_valid_loss})
+            print(
+                f"\nEpoch {epoch + 1}/{n_epochs}, mean valid loss "
+                f"{val_name} {mean_valid_loss:.4f}",
+            )
 
-    # Device
+        # Free unused VRAM
+        torch.cuda.empty_cache()
+        # Eventually update learning rate
+        scheduler.step()
 
+        # Save model
+        if (epoch + 1) % train_config["save_interval"] == 0:
+            torch.save(
+                model.state_dict(),
+                f"../models/{archi_name}/{run_id}/{run_id}_ep{epoch + 1}.pth",
+            )
+            print(
+                f"Model saved to ../models/{archi_name}/{run_id}/"
+                f"{run_id}_ep{epoch + 1}.pth",
+            )
+
+    # Save final model
+    torch.save(model.state_dict(), f"../models/{archi_name}/{run_id}/{run_id}_last.pth")
+    print(f"Model saved to ../models/{archi_name}/{run_id}/{run_id}_last.pth")
+
+
+def run(config: Dict) -> None:
+    """Run training."""
     device = torch.device(
         "cuda"
         if torch.cuda.is_available()
@@ -308,56 +266,45 @@ if __name__ == "__main__":
         if torch.backends.mps.is_built()
         else "cpu",
     )
-
-    # Model
-
-    model = Scandium(config["model"])
-
-    # Data loaders
+    archi_name = config["archi_name"]
+    archi_name_cap = "".join([word.capitalize() for word in archi_name.split("_")])
+    try:
+        model_class = getattr(models_module, archi_name_cap)
+    except AttributeError as exc:
+        raise ValueError(f"Model {archi_name_cap} not found in "
+                         "lnb/architecture/models.py") from exc
+    model = model_class(config["model"]).to(device)
+    # Print number of parameters
+    n_params = sum(params.numel() for params in model.parameters()
+                   if params.requires_grad)
+    print(f"Run id {config['run_id']}, architecure {archi_name}")
+    print(f"Model has {n_params} trainable parameters")
 
     train_dataloader = DataLoader(
-        LNBDataset(mask_fn=mask_fn, **config["data"]), **config["dataloader"],
+        LNBDataset(mask_fn=mask_fn, **config["data"]),
+        **config["dataloader"],
     )
-
     # Build validation data loaders
     val_data_config = config["data"].copy()
-    val_data_config["grid_augmentation"] = False  # No augmentation for validation
-
     val_loader_config = config["dataloader"].copy()
+    val_data_config["grid_augmentation"] = False  # No augmentation for validation
     val_loader_config["shuffle"] = False  # No shuffle for validation
-    val_loader_config["batch_size"] = 16  # Hard-coded batch size for validation
-
-    val_dataloaders = {}
-    for name in ["generalisation", "regular", "mask_cloudy"]:
+    val_loader_config["batch_size"] = config['train']['val_batch_size']
+    val_dataloaders = []
+    for name in ["regular", "cloudy"]:
         val_data_config["name"] = name
         val_data_config["csv_name"] = f"validation_{name}.csv"
         val_dataloader = DataLoader(
-            LNBDataset(mask_fn=mask_fn, **val_data_config), **val_loader_config,
+            LNBDataset(mask_fn=mask_fn, **val_data_config),
+            **val_loader_config,
         )
-        val_dataloaders["validation_" + name] = val_dataloader
-
-    dataloaders = {
-        "train": train_dataloader,
-        "validation": val_dataloaders,
-    }
-
-    # Train
-
-    run_id = max(int(name) for name in os.listdir(
-        "../models/strontium") if not name.startswith('.')) + 1
-    config["run_id"] = run_id
-
-    wandb.init(
-        project="lnb", entity="leaf_nothing_behind", group="strontium", config=config,
-    )
-
-    trainer = Trainer(
-        model=model,
-        dataloaders=dataloaders,
+        val_dataloaders.append(val_dataloader)
+    # Run training
+    train_val_loop(
         config=config,
+        model=model,
+        train_dataloader=train_dataloader,
+        val_dataloaders=val_dataloaders,
         device=device,
-        process_func=parse_data_device,
+        archi_name=config['archi_name'],
     )
-    trainer.run()
-
-    wandb.finish()
