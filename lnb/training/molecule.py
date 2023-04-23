85d482e6a23a09ea168ec2c29e7528aa5640e16b
Valentin Goldité <valentin.goldite@gmail.com>
2023-04-24 01:23:45 +0200

    :fire: Remove legacy code

--- lnb/training/molecule.py
+++ lnb/training/molecule.py
@@ -1,7 +1,6 @@
-"""Training functions for Scandium."""
+"""Training functions for Strontium."""
 import argparse
 import os
-from time import time
 from typing import Dict, List, Tuple
 
 import numpy as np
@@ -12,9 +11,9 @@ from torch.optim.lr_scheduler import MultiStepLR
 from torch.utils.data import DataLoader
 
 import wandb
-from lnb.architecture.models import Scandium
+from lnb.architecture.cloud_models import Molecule
+from lnb.architecture.models import Strontium
 from lnb.data.dataset import LNBDataset
-from lnb.training.log_utils import get_time_log
 from lnb.training.metrics import mse_loss
 
 ParsedDataType = Dict[str, Dict[str, torch.Tensor]]
@@ -69,33 +68,27 @@ def train_step(
     optimizer: torch.optim.Optimizer,
     parsed_data: ParsedDataType,
     interm_supervis: bool,
+    weight_2: float = 0.2,
+    weight_3: float = 0.2,
 ) -> Tuple:
     """Perform a training step."""
     # Forward pass and loss computation
     optimizer.zero_grad()
-    lai_pred, lai_pred2 = model(**parsed_data["input_data"])
+    lai_pred, lai_interm = model(**parsed_data["input_data"])
     loss = mse_loss(lai_pred=lai_pred, **parsed_data["target_data"])
     if interm_supervis:
-        lai_target_interm = torch.cat(
-            [
-                parsed_data["input_data"]["in_lai"],
-                parsed_data["target_data"]["lai_target"].unsqueeze(1),
-            ],
-            dim=1,
-        )
-        lai_target_mask_interm = torch.cat(
-            [
-                parsed_data["input_data"]["in_mask_lai"][:, :, 0:1],
-                parsed_data["target_data"]["lai_target"].unsqueeze(1),
-            ],
-            dim=1,
-        )
+        lai_pred2, lai_pred3 = lai_interm
+
         loss2 = mse_loss(
             lai_pred=lai_pred2,
-            lai_target=lai_target_interm,
-            lai_target_mask=lai_target_mask_interm,
+            **parsed_data["target_data"],
         )
-        loss_total = loss + loss2
+        loss3 = mse_loss(
+            lai_pred=lai_pred3,
+            **parsed_data["target_data"],
+        )
+
+        loss_total = loss + weight_2 * loss2 + weight_3 * loss3
     else:
         loss2 = None
         loss_total = loss
@@ -124,15 +117,15 @@ def train_val_loop(
     """Training and validation loop."""
     # Save config
     run_id = config["run_id"]
-    os.makedirs(f"../models/scandium/{run_id}", exist_ok=True)
+    os.makedirs(f"../models/cloud_strontium/{run_id}", exist_ok=True)
     with open(
-        f"../models/scandium/{run_id}/config.yaml", "w", encoding="utf-8",
+        f"../models/cloud_strontium/{run_id}/config.yaml", "w", encoding="utf-8",
     ) as cfg_file:
         yaml.dump(dict(wandb.config), cfg_file)
     # Get training config params
     train_config = config["train"]
     n_epochs = train_config["n_epochs"]
-    n_batch = len(train_dataloader)
+    # n_batch = len(train_dataloader)
     lr_decay = train_config["learning_rate_decay"]
     lr_n_mult = train_config["learning_rate_n_mult"]
     # Optimizer and scheduler
@@ -141,41 +134,46 @@ def train_val_loop(
     scheduler = MultiStepLR(optimizer, milestones=milestones, gamma=lr_decay)
 
     model = model.to(device)
-    start_t = time()
+    # start_t = time()
     for epoch in range(n_epochs):
         # Training
         model = model.train()
-        epoch_start_t = time()
-        train_losses = []
-        i_batch = 0  # iteration number in current epoch
-        for data, glob in train_dataloader:
-            i_batch += 1
-            # Parse data and put it on device
-            parsed_data = parse_data_device(data, glob, device)
-            loss, loss2 = train_step(
-                model, optimizer, parsed_data, train_config["interm_supervis"],
-            )
-            train_losses.append(loss.item())
-            # Logs
-            wandb.log({"train loss": loss.item()})
-            if loss2 is not None:
-                wandb.log({"train loss2": loss2.item()})
-            if i_batch % train_config["log_interval"] == 0:
-                current_t = time()
-                eta_str, eta_ep_str = get_time_log(
-                    current_t, start_t, epoch_start_t, i_batch, epoch, n_batch,
-                    n_epochs,
-                )
-                print(
-                    f"train loss batch: {loss.item():.4f} - eta epoch {eta_ep_str}"
-                    f"- eta {eta_str}     ",
-                    end="\r",
-                )
+        # epoch_start_t = time()
+        # train_losses = []
+        # i_batch = 0  # iteration number in current epoch
+        # for data, glob in train_dataloader:
+        #     i_batch += 1
+        #     # Parse data and put it on device
+        #     parsed_data = parse_data_device(data, glob, device)
+        #     loss, loss2 = train_step(
+        #         model,
+        #         optimizer,
+        #         parsed_data,
+        #         train_config["interm_supervis"],
+        #         train_config["weight_2"],
+        #         train_config["weight_3"],
+        #     )
+        #     train_losses.append(loss.item())
+        #     # Logs
+        #     wandb.log({"train loss": loss.item()})
+        #     if loss2 is not None:
+        #         wandb.log({"train loss2": loss2.item()})
+        #     if i_batch % train_config["log_interval"] == 0:
+        #         current_t = time()
+        #         eta_str, eta_ep_str = get_time_log(
+        #             current_t, start_t, epoch_start_t, i_batch, epoch, n_batch,
+        #             n_epochs,
+        #         )
+        #         print(
+        #             f"train loss batch: {loss.item():.4f} - eta epoch {eta_ep_str}"
+        #             f"- eta {eta_str}     ",
+        #             end="\r",
+        #         )
 
-        # Epoch train logs
-        mean_train_loss = sum(train_losses) / len(train_losses)
-        wandb.log({"mean train loss": mean_train_loss})
-        print(f"\nEpoch {epoch + 1}/{n_epochs}, mean train loss {mean_train_loss:.4f}")
+        # # Epoch train logs
+        # mean_train_loss = sum(train_losses) / len(train_losses)
+        # wandb.log({"mean train loss": mean_train_loss})
+        # print(f"\nEpoch {epoch + 1}/{n_epochs}, mean train loss {mean_train_loss:.4f}")
 
         # Validation
         model = model.eval()
@@ -188,6 +186,7 @@ def train_val_loop(
             i_batch = 0
             for data, glob in val_dataloader:
                 i_batch += 1
+                print(i_batch)
                 # Parse data and put it on device
                 parsed_data = parse_data_device(data, glob, device)
                 loss = valid_step(model, parsed_data)
@@ -216,25 +215,37 @@ def train_val_loop(
         if (epoch + 1) % train_config["save_interval"] == 0:
             torch.save(
                 model.state_dict(),
-                f"../models/scandium/{run_id}/{run_id}_ep{epoch + 1}.pth",
+                f"../models/cloud_strontium/{run_id}/{run_id}_ep{epoch + 1}.pth",
             )
             print(
-                f"Model saved to ../models/scandium/{run_id}/"
+                f"Model saved to ../models/cloud_strontium/{run_id}/"
                 f"{run_id}_ep{epoch + 1}.pth",
             )
 
     # Save final model
-    torch.save(model.state_dict(), f"../models/scandium/{run_id}/{run_id}_last.pth")
-    print(f"Model saved to ../models/scandium/{run_id}/{run_id}_last.pth")
+    torch.save(model.state_dict(),
+               f"../models/cloud_strontium/{run_id}/{run_id}_last.pth")
+    print(f"Model saved to ../models/cloud_strontium/{run_id}/{run_id}_last.pth")
 
 
 def run(config: Dict) -> None:
     """Run training."""
-    model = Scandium(config["model"])
-    # Print number of parameters
-    n_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
-    print(f"Model has {n_params} parameters")
-
+    device = torch.device(
+        "cuda"
+        if torch.cuda.is_available()
+        else "mps"
+        if torch.backends.mps.is_built()
+        else "cpu",
+    )
+    base_model = Strontium(config["model"])
+    base_model.load_state_dict(
+        torch.load("../models/strontium/16062004/16062004_last.pth"),
+    )
+    cloud_model = Strontium(config["model"])
+    cloud_model.load_state_dict(
+        torch.load("../models/cloud_strontium/2/2_last.pth"),
+    )
+    model = Molecule(base_model, cloud_model).to(device)
     train_dataloader = DataLoader(
         LNBDataset(mask_fn=mask_fn, **config["data"]), **config["dataloader"],
     )
@@ -243,23 +254,15 @@ def run(config: Dict) -> None:
     val_loader_config = config["dataloader"].copy()
     val_data_config["grid_augmentation"] = False  # No augmentation for validation
     val_loader_config["shuffle"] = False  # No shuffle for validation
-    val_loader_config["batch_size"] = 16  # Hard-coded batch size for validation
+    val_loader_config["batch_size"] = 1  # Hard-coded batch size for validation
     val_dataloaders = []
-    for name in ["regular", "mask_cloudy"]:
+    for name in ["mask_cloudy", "regular"]:
         val_data_config["name"] = name
         val_data_config["csv_name"] = f"validation_{name}.csv"
         val_dataloader = DataLoader(
             LNBDataset(mask_fn=mask_fn, **val_data_config), **val_loader_config,
         )
         val_dataloaders.append(val_dataloader)
-
-    device = torch.device(
-        "cuda"
-        if torch.cuda.is_available()
-        else "mps"
-        if torch.backends.mps.is_built()
-        else "cpu",
-    )
     # Run training
     train_val_loop(
         config=config,
@@ -274,18 +277,19 @@ def main() -> None:
     """Main function to run a train with wandb."""
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--config_path", type=str, required=False, default="config/scandium/base.yaml",
+        "--config_path", type=str, required=False,
+        default="config/strontium/exp.yaml",
     )
     args = parser.parse_args()
 
     with open(args.config_path, encoding="utf-8") as cfg_file:
         config = yaml.safe_load(cfg_file)
     # New id (for model name)
-    run_id = max(int(name) for name in os.listdir(
-        "../models/scandium") if not name.startswith('.')) + 1
+    run_id = max(int(name) for name in os.listdir("../models/cloud_strontium")) + 1
     config["run_id"] = run_id
     wandb.init(
-        project="lnb", entity="leaf_nothing_behind", group="scandium", config=config,
+        project="lnb", entity="leaf_nothing_behind",
+        group="molecule", config=config,
     )
     run(dict(wandb.config))
     wandb.finish()
